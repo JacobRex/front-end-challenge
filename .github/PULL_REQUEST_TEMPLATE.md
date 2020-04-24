b2915bd716dadeda4bc025bf07c1fb7245e6d2b0
Jacob Rex <jacobrex@gmail.com>
2020-04-24 08:32:54 -0500

    [Architechture] Setting up repo (#1)
    * installing nuxt
    
    * Setting up eslint
    
    * Setting up a PR template
    
    * Installing postcss plugins
    
    * Adding aliases to nuxtconfig
    
    * Adding missing plugin for linting jest

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,20 @@
+Prepend your PR title with one of the following:
+- `Architecture:` for changes to the build or deployment.
+- `Changed:` for changes in existing functionality.
+- `Component:` for new components.
+- `Feature:` for new features.
+- `Fixed:` for any bug fixes.
+- `Internal:` for changes to the repo.
+- `Removed:` for deprecated features.
+
+### Problem
+<!-- Describe the problem -->
+
+**Designs:** <!-- Link -->
+
+### Description
+<!-- Describe the changes made in this PR -->
+
+---
+
+<!-- Finished screenshots (if necessary): -->
---
+++ nuxt.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+
+export default {
+  mode: "spa",
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Setup Aliases
+      config.resolve.alias = {
+        Assets: path.resolve(__dirname, 'assets'),
+        Components: path.resolve(__dirname, 'components'),
+        Pages: path.resolve(__dirname, 'pages'),
+      };
+
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/,
+        });
+      }
+    },
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-import': {},
+        'postcss-nesting': {},
+        'postcss-custom-media': {},
+        'postcss-preset-env': {
+          browsers: 'last 2 versions',
+        },
+        'cssnano': {},
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        }
+      }
+    }
+  },
+};
